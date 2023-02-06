d8722ef9e724ee8f222d1c2d0afa55748dd5932d
meixingc <realmeixing@gmail.com>
2023-02-06 16:04:11 -0500

    flag shows

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const holidayUrl = "https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022";
 const flagUrl = "https://holidayapi.com/v1/countries?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701";
-let userInput = document.querySelector('#country-code').values;
 let flagIMG = document.querySelector('#flag');
-let code = document.querySelector('#country-code');
+const error = document.querySelector('.err')
+const search = document.querySelector('#searchButton');
+
+
+
+function searched() {
+    error.innerText = '';
+    let userCode = document.querySelector('#country-code').value;
+    if (userCode.length === 0){
+        // error.style.display = 
+        error.innerText = 'Error! Please enter a country code.';
+    }
+    else {
+        flagIMG.style.opacity = '1';
+        flagIMG.src = `https://www.countryflagicons.com/FLAT/64/${userCode}.png`
+    }
+}
+    
+
+search.addEventListener('click', searched);
+
+
+
+
+
 // async function getData (event) {
 //     event.preventDefault();
 
@@ -18,14 +41,15 @@ let code = document.querySelector('#country-code');
 //         console.log('ugh', nope)
 //     })
 // }
-flagIMG.src = `https://www.countryflagicons.com/FLAT/64/${code}.png`
-fetch(flagUrl)
-.then(res => {
-    return res.json()
-})
-.then(res => {
-    console.log('yay', res)
-})
-.catch(nope => {
-    console.log('ugh', nope)
-})
+
+// fetch(flagUrl)
+// .then(res => {
+//     return res.json()
+// })
+// .then(res => {
+//     console.log('yay', res)
+// })
+// .catch(nope => {
+//     console.log('ugh', nope)
+// })
+
