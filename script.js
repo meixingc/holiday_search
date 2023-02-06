8a36d94f764f25df70f82f8fd5420bb3c130480e
meixingc <realmeixing@gmail.com>
2023-02-06 17:21:53 -0500

    name and date js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const holidayUrl = "https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022";
-const flagUrl = "https://holidayapi.com/v1/countries?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701";
+
 let flagIMG = document.querySelector('#flag');
 const error = document.querySelector('.err')
 const search = document.querySelector('#searchButton');
 
-
-
-function searched() {
+async function searched() {
     error.innerText = '';
     let userCode = document.querySelector('#country-code').value;
+    let name = document.querySelector('#holiday-name').value;
+    let date = document.querySelector('#date').value;
     if (userCode.length === 0){
-        // error.style.display = 
         error.innerText = 'Error! Please enter a country code.';
     }
     else {
-        flagIMG.style.opacity = '1';
+        flagIMG.style.display = 'flex';
         flagIMG.src = `https://www.countryflagicons.com/FLAT/64/${userCode}.png`
+        document.querySelector('#country-code').value = "";
+        
+        if (name.length != 0){
+            let holidayUrl = `https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=${userCode}&year=2022&search=${name}`;
+            document.querySelector('#holiday-name').value = '';
+        }
+        else if (date.length != 0){
+            let month = date.slice(0,2);
+            let day = date.slice(3,5);
+            let holidayUrl = `https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=${userCode}&year=2022&month=${month}&day=${day}`;
+            document.querySelector('#date').value = '';
+        }
+        else {
+            let holidayUrl = `https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=${userCode}&year=2022&month=${month}&day=${day}`
+        }
     }
-}
+    }
+    
+
     
 
 search.addEventListener('click', searched);
