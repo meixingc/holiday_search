d822569752b17f4cd8e79a1c3f8ba225fd0a8172
meixingc <realmeixing@gmail.com>
2023-02-06 13:17:48 -0500

    css edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022"
+const url = "https://holidayapia.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022"
 
 fetch(url)
     .then(res => {
