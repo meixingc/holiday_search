54c3cad9e6acaf857444bffe6a67802f7e591667
meixingc <realmeixing@gmail.com>
2023-02-06 14:28:39 -0500

    nicer looking

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const url = "https://holidayapia.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022"
+// const holidayUrl = "https://holidayapi.com/v1/holidays?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701&country=US&year=2022";
+// const flagUrl = "https://holidayapi.com/v1/countries?pretty&key=d194ebca-ea3d-4c62-84f4-0898e8fc9701";
+// let userInput = document.querySelector('#inputBar').values;
+// async function getData (event) {
+//     event.preventDefault();
+// fetch(holidayUrl)
+//     .then(res => {
+//         return res.json()
+//     })
+//     .then(res => {
+//         console.log('yay', res)
+//     })
+//     .catch(nope => {
+//         console.log('ugh', nope)
+//     })
+// }
 
-fetch(url)
-    .then(res => {
-        return res.json()
-    })
-    .then(res => {
-        console.log('yay', res)
-    })
-    .catch(nope => {
-        console.log('ugh', nope)
-    })
+// fetch(flagUrl)
+// .then(res => {
+//     return res.json()
+// })
+// .then(res => {
+//     console.log('yay', res)
+// })
+// .catch(nope => {
+//     console.log('ugh', nope)
+// })
